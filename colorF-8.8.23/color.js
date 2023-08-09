2c9219f6ec59d4de6ca9014a72a56ebee48796ab
Nicholas Jackman <njackman007@gmail.com>
2023-08-08 22:28:54 -0700

    first pr Day 4..working

---
+++ colorF-8.8.23/color.js
@@ -0,0 +1,16 @@
+function flip() {
+  const yellow = document.getElementsByClassName("yellow");
+  const blue = document.getElementsByClassName("blue");
+  const black = document.getElementsByClassName("black");
+  const green = document.getElementsByClassName("green");
+  const btnElement = document.getElementById("btn");
+  yellow.addEventListener("click", () => {
+    <style background-color:yellow></style>;
+  });
+  blue.addEventListener("click", () => {
+    <style background-color:blue></style>;
+  });
+  black.addEventListener("click", () => {});
+  green.addEventListener("click", () => {});
+}
+flip();
