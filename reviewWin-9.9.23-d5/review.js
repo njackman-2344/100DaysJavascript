175047e02158b74cc7589a841a0d2229b49083e4
Nicholas Jackman <njackman007@gmail.com>
2023-08-09 17:32:58 -0700

    day 5, working state iCodeThis

---
+++ reviewWin-9.9.23-d5/review.js
@@ -0,0 +1,10 @@
+function form() {
+  const inputTitle = document.getElementsByClassName("reviewTitle").textContent;
+  const inputRI =
+    document.getElementsByClassName("textReviewInput").textContent;
+  const inputNickname = document.getElementsByClassName("nickname").textContent;
+  const inputEmail = document.getElementsByClassName("email").textContent;
+  const radio = document.getElementsByClassName("yes/no").value;
+
+  const btn = document.getElementsByClassName("btnSubmit");
+}
